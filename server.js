de4ef0557ba3bdf65746ed33d5dec3246c5ce366
sanskarkhare <sanskarkhare47@gmail.com>
2021-02-09 15:02:04 +0545

    cors3

--- server.js
+++ server.js
@@ -15,6 +15,15 @@ const transactions = require('./routes/transactions')
 
 const app = express();
 app.use(cors());
+
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
 app.use(express.json())
 
 if(process.env.NODE_ENV === 'development') {
